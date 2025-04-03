190a6a9bd4bd9399d3ae30f70e0b2b6fb30c0451
orion <11010101@11000110.xyz>
2025-04-02 23:33:36 -0700

    Update Project2.ino

--- Project2/Project2.ino
+++ Project2/Project2.ino
@@ -1,6 +1,28 @@
+/**
+ * Simon Says
+ *
+ * Author: Shygaan Khalili
+ * Date: Apr-02-2025
+ * Course: COMP 120
+ *
+ *
+ * Description:
+ * Arduino-based implementation of "Simon Says". Generates random pattern of LED flashes that player must
+ * memorize and replicate by pressing corresponding buttons. Each round, pattern increases in length.
+ * Game continues until the player makes an error or reaches the maximum
+ * pattern length.
+ * 
+ * 
+ * Hardware Requirements:
+ * - 2 LEDs
+ * - 2 Buttons
+ * - 1 buzzer
+ * - Arduino-compatible MCU (I used STM32)
+ */
+
+#define LED_1 A1
 #define BUTTON_1 A2
 #define BUTTON_2 A3
-#define LED_1 A1
 #define LED_2 A4
 #define PIEZO A5
 
@@ -17,7 +39,7 @@
  * @param pattern Array to store the generated pattern.
  * @param length Length of the pattern to generate.
  */
-void generatePattern(int *pattern, int length);
+void generatePattern(int* pattern, int length);
 
 /**
  * @brief Displays the generated pattern using LEDs.
@@ -86,7 +108,7 @@ void setup() {
   pinMode(BUTTON_1, INPUT);
   pinMode(BUTTON_2, INPUT);
 
-  randomSeed(analogRead(A0)); // Initialize random number generator with noise
+  randomSeed(analogRead(A0));  // Initialize random number generator with noise
 
   Serial.begin(9600);
   Serial.println("Welcome to Simon Says!");
@@ -105,8 +127,8 @@ void loop() {
 
   if (!processPlayerSequence(pattern, patternLength)) {
     gameOver();
-    patternLength = 1; // Reset game
-    return; // Break loop
+    patternLength = 1;  // Reset game
+    return;             // Break loop
   }
 
   playWinTune();
@@ -124,11 +146,11 @@ void loop() {
 }
 
 
-void generatePattern(int *pattern, int length) {
+void generatePattern(int* pattern, int length) {
   pattern[length - 1] = random(1, 3);
 }
 
-void displayPattern(int *pattern, int length) {
+void displayPattern(int* pattern, int length) {
   Serial.println("Watch the pattern!");
   for (int i = 0; i < length; i++) {
     int ledPin = (pattern[i] == 1) ? LED_1 : LED_2;
@@ -139,7 +161,7 @@ void displayPattern(int *pattern, int length) {
   }
 }
 
-bool processPlayerSequence(int *pattern, int length) {
+bool processPlayerSequence(int* pattern, int length) {
   int playerTurn = 0;
   unsigned long startTime = millis();
 
@@ -160,7 +182,7 @@ bool processPlayerSequence(int *pattern, int length) {
   return true;
 }
 
-bool validatePlayerInput(int *pattern, int button, int index) {
+bool validatePlayerInput(int* pattern, int button, int index) {
   if (pattern[index] != button) {
     Serial.println("Wrong input!");
     return false;
@@ -172,7 +194,8 @@ bool isButtonPressed(int buttonPin, int ledPin) {
   digitalWrite(ledPin, HIGH);
   delay(DEBOUNCE_DELAY);
   if (digitalRead(buttonPin) == HIGH) {
-    while (digitalRead(buttonPin) == HIGH);
+    while (digitalRead(buttonPin) == HIGH)
+      ;
     digitalWrite(ledPin, LOW);
     return true;
   }
@@ -187,20 +210,20 @@ void gameOver() {
 }
 
 void playStartupTune() {
-  int notes[] = {262, 330, 392, 523};
-  int durations[] = {200, 200, 200, 400};
+  int notes[] = { 262, 330, 392, 523 };
+  int durations[] = { 200, 200, 200, 400 };
   playTune(notes, durations, 4);
 }
 
 void playWinTune() {
-  int winNotes[] = {523, 587, 659, 698};
-  int durations[] = {150, 150, 150, 300};
+  int winNotes[] = { 523, 587, 659, 698 };
+  int durations[] = { 150, 150, 150, 300 };
   playTune(winNotes, durations, 4);
 }
 
 void playGameOverTune() {
-  int notes[] = {400, 300};
-  int durations[] = {300, 300};
+  int notes[] = { 400, 300 };
+  int durations[] = { 300, 300 };
   playTune(notes, durations, 2);
 }
 
