0d473932f4a46cb4d9ab5e007c5e0a5d526caae4
0xA79D2E <167731823+0xA79D2E@users.noreply.github.com>
2025-04-07 12:54:41 -0700

    Create main.c

---
+++ Linear/main.c
@@ -0,0 +1,27 @@
+
+/**
+ * Linear search algorithm
+ * Author: Shygaan Khalili
+ */
+#include <stdio.h>
+
+int linearSearch(int* array, int length, int target) {
+
+    for (int i = 0; i < length; ++i) {
+        if (array[i] == target) return i;
+    } return -1;
+
+}
+
+
+int main() {
+
+    int array[] = {9,7,4,8,2,6,1,3,5};
+
+    int x = linearSearch(array, sizeof(array), 6);
+
+    if (x == -1) printf("Element not found.\n");
+    else printf("Element found at index = %x\n", x);
+
+    return 0;
+}
